0b5dcd75d7fa4cc87be762e08a697e40ec4fb00f
lulexyz <78207010+lulexyz@users.noreply.github.com>
2024-05-30 22:52:54 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-# 检查输入参数
-# 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <port> <ports_number> [username] [password]"
-    exit 1
-elif [ $# -eq 2 ]; then
-    xray_port=$1
-    ports_number=$2
-    authtype="noauth"
-elif [ $# -eq 4 ]; then
-    xray_port=$1
-    ports_number=$2
-    username=$3
-    password=$4
-    authtype="password"
-else
-    echo "Usage: $0 <port> <ports_number> [username] [password]"
+# 检查输入参数,如何输入参数如果不是4，就打印提示。如果输入为4，就是设置的socks端口，socks代理的ip数量，访问的用户名和密码
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 <port:6666> <ports_number:20> <user> <password>"
     exit 1
 fi
+
+myport=$1
+ports_number=$2
+user=$3
+password=$4
+authtype="password"
 
 # 检查配置文件是否存在
 if [ ! -d "config" ]; then
@@ -57,43 +49,48 @@ fi
 
 ## 生成 torrc 配置文件的动态部分
 start_port=58001
-end_port=$((start_port + ports_number - 1))
-
-ports_config=""
-for ((port = $start_port; port <= $end_port; port++)); do
-    printf -v ports_config '%s%s' "$ports_config" "SOCKSPort 0.0.0.0:$port" $'\n'
+for (( i=0; i<$ports_number; i++ ))
+do
+    port=$((start_port + i))
+    ports_config+="SOCKSPort 0.0.0.0:$port\n"
 done
+# Remove the trailing newline character from ports_config
+ports_config=$(echo -e "$ports_config" | sed -e '$a\')
+
 
 # 将动态生成的内容与静态内容合并并写入文件
 cat > config/torrc <<EOL
 ${ports_config}
-NewCircuitPeriod 30
-CircuitBuildTimeout 10
+NewCircuitPeriod 30 #对于每个端口来说，每30秒重新创建一个新链路，也就是换一个新IP
+CircuitBuildTimeout 10 #对于新建每个链路的过程来说，建立程序超过10秒则直接放弃，保障了连接到线路的质量
+Log notice file /var/log/tor/notices.log
+DataDirectory /var/lib/tor
+ControlPort 9052
 %include /etc/torrc.d/*.conf
 EOL
 echo "Generated torrc with ports starting from $start_port for $ports_number ports."
 
-
-# 生成config.json
-
-# 生成服务器列表，最后一个端口不添加逗号
-servers_list=""
-for ((port = $start_port; port <= $end_port; port++)); do
-    if [[ $port -eq $end_port ]]; then
-        # 最后一个端口，不添加逗号
-        printf -v servers_list '%s%s' "$servers_list" "{\"address\": \"tor-privoxy\", \"port\": $port}"
+## 生成 config.json 配置文件的动态部分
+for (( i=0; i<$ports_number; i++ ))
+do
+    port=$((start_port + i))
+    if [ $i -eq $((ports_number - 1)) ]; then
+        ports_config_json+="{\n    \"address\": \"tor-privoxy\",\n    \"port\": $port\n  }\n"
     else
-        # 非最后一个端口，添加逗号和换行
-        printf -v servers_list '%s%s' "$servers_list" "{\"address\": \"tor-privoxy\", \"port\": $port},"
+        ports_config_json+="{\n    \"address\": \"tor-privoxy\",\n    \"port\": $port\n  },\n"
     fi
-    printf -v servers_list '%s%s' "$servers_list" $'\n'
 done
+# Remove the trailing comma and newline
+ports_config_json=$(echo -e "$ports_config_json" | sed '$s/,$//')
 
-# 使用EOL将静态部分和动态生成的服务器列表合并到config.json中
 cat > config/config.json <<EOL
 {
   "api": {
-    "services": ["HandlerService", "LoggerService", "StatsService"],
+    "services": [
+      "HandlerService",
+      "LoggerService",
+      "StatsService"
+    ],
     "tag": "api"
   },
   "dns": null,
@@ -103,54 +100,126 @@ cat > config/config.json <<EOL
       "listen": "127.0.0.1",
       "port": 62789,
       "protocol": "dokodemo-door",
-      "settings": {"address": "127.0.0.1"},
+      "settings": {
+        "address": "127.0.0.1"
+      },
+      "sniffing": null,
+      "streamSettings": null,
       "tag": "api"
     },
     {
       "listen": null,
-      "port": $xray_port,
+      "port": $myport,
       "protocol": "socks",
       "settings": {
-        "accounts": [{"pass": "${password}", "user": "${username}"}],
-        "auth": "${authtype}",
+        "accounts": [
+          {
+            "pass": "$password",
+            "user": "$user"
+          }
+        ],
+        "auth": "$authtype",
         "ip": "127.0.0.1",
         "udp": false
       },
-      "tag": "inbound-socks"
+      "sniffing": null,
+      "streamSettings": null,
+      "tag": "inbound-31445"
     }
   ],
-  "log": {"error": "./error.log", "loglevel": "warning"},
+  "log": {
+    "error": "./error.log",
+    "loglevel": "warning"
+  },
   "outbounds": [
-    {"protocol": "freedom", "settings": {}, "tag": "direct"},
+    {
+      "protocol": "freedom",
+      "settings": {}
+    },
     {
       "protocol": "socks",
       "settings": {
         "servers": [
-$servers_list
+          $ports_config_json
         ]
       },
       "tag": "socks_out"
     },
-    {"protocol": "blackhole", "settings": {}, "tag": "blocked"}
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "blocked"
+    }
   ],
+  "policy": {
+    "levels": {
+      "0": {
+        "statsUserDownlink": true,
+        "statsUserUplink": true
+      }
+    },
+    "system": {
+      "statsInboundDownlink": true,
+      "statsInboundUplink": true
+    }
+  },
+  "reverse": null,
   "routing": {
     "domainStrategy": "IPIfNonMatch",
     "rules": [
-      {"inboundTag": ["api"], "outboundTag": "api", "type": "field"},
-      {"domain": ["regexp:.*"], "outboundTag": "socks_out", "type": "field"},
-      {"ip": ["0.0.0.0/0", "::/0"], "outboundTag": "socks_out", "type": "field"},
-      {"ip": ["geoip:private"], "outboundTag": "blocked", "type": "field"},
-      {"outboundTag": "blocked", "protocol": ["bittorrent"], "type": "field"}
+      {
+        "inboundTag": [
+          "api"
+        ],
+        "outboundTag": "api",
+        "type": "field"
+      },
+      {
+        "domain": [
+          "regexp:.*"
+        ],
+        "outboundTag": "socks_out",
+        "type": "field"
+      },
+      {
+        "ip": [
+          "0.0.0.0/0",
+          "::/0"
+        ],
+        "outboundTag": "socks_out",
+        "type": "field"
+      },
+      {
+        "ip": [
+          "geoip:private"
+        ],
+        "outboundTag": "blocked",
+        "type": "field"
+      },
+      {
+        "outboundTag": "blocked",
+        "protocol": [
+          "bittorrent"
+        ],
+        "type": "field"
+      }
     ]
-  }
+  },
+  "stats": {},
+  "transport": null
 }
 EOL
 
-echo "config.json has been generated with dynamic port mappings."
+for (( i=0; i<$ports_number; i++ ))
+do
+    port=$((start_port + i))
+    tor_ports+="      - \"127.0.0.1:$port:$port\" \n"
+done
 
+# Remove the trailing newline character from ports_config
+tor_ports=$(echo -e "$tor_ports" | sed -e '$a\')
 
-
-# 生成 Docker Compose 文件
+# Generate the docker-compose.yml file
 cat > docker-compose.yml <<EOL
 version: '3'
 
@@ -164,21 +233,13 @@ services:
     tty: true
     restart: unless-stopped
     ports:
-      - "$port:$port"
+      - $myport:$myport
+
+  tor-privoxy:
+    restart: always
+    image: peterdavehello/tor-socks-proxy:latest
+    volumes:
+      - ./config/torrc:/etc/tor/torrc
+    ports:
+$tor_ports
 EOL
-
-# 添加tor-privoxy服务并动态生成端口映射
-echo "  tor-privoxy:" >> docker-compose.yml
-echo "    restart: always" >> docker-compose.yml
-echo "    image: peterdavehello/tor-socks-proxy:latest" >> docker-compose.yml
-echo "    volumes:" >> docker-compose.yml
-echo "      - ./config/torrc:/etc/tor/torrc" >> docker-compose.yml
-echo "    ports:" >> docker-compose.yml
-
-for ((p = $start_port; p <= $end_port; p++)); do
-    echo "      - \"127.0.0.1:$p:$p\" # Tor SOCKS proxy" >> docker-compose.yml
-done
-
-# 清理并启动 Docker 容器
-docker-compose down
-docker-compose up -d
